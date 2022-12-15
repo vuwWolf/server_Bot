2a5ae13e8d15f8cf36cae99307f70ac258ed6f2b
White_Wolf <63950114+vuwWolf@users.noreply.github.com>
2022-12-15 22:02:33 +0300

    penis

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from vkbottle.bot import Bot, Message # Импорт библиотеки
 
 TokenPenis = input("Введите токен: ")
-bot = Bot(token=TokenPenis) # Вместо TokenPenis ввести токен (не забудь, что кавычки обязательны)
+bot = Bot(token=TokenPenis)
+
 
 @bot.on.message(text="/help") # Задаю хуйню, чтобы при появлении "/help" в чате, писалось сообщение
 async def help_handler(message: Message):
@@ -13,19 +14,20 @@ async def help_handler(message: Message):
                          '/staff - разрабы сервера'
                          ) # Сам текст, {users_info[0].id} - айди страницы человека, {users.info[0].first_name} - имя
 
+
 @bot.on.message(text="/viewmap")
 async def map_handler(message: Message):
     users_info = await bot.api.users.get(message.from_id)
     await message.answer(f"[id{users_info[0].id}|{users_info[0].first_name}], карта сервера: http://n21.joinserver.ru:25663/")
+
 
 @bot.on.message(text="/staff")
 async def staff_handler(message: Message):
     users_info = await bot.api.users.get(message.from_id)
     await message.answer(f'[id{users_info[0].id}|{users_info[0].first_name}], вот все разработчики сервера:\n'
                          '\n'
-                         'Антон "shmgn" Шмагин (https://vk.com/aantoss) - ХУЙ\n'
-                         'Паша "ХУЙ" Одинцов (https://vk.com/errormaned) - ХУЙ'
-                         )
+                         'Антон Шмагин (https://vk.com/aantoss) и Павел Одинцов (https://vk.com/errormaned)')
+
 
 @bot.on.message(text="Лобанов, пососи-ка мой пятиярдовый хуй") # пасхалОЧКА
 async def lobanov_handler(message: Message):
