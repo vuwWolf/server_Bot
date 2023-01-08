992b22c9c603a5a63db62c67ee122c67d968f233
White_Wolf <63950114+vuwWolf@users.noreply.github.com>
2023-01-08 16:59:53 +0300

    добавлена система "ПО ПРИКАЗУ"

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 from vkbottle.bot import Bot, Message # Импорт библиотеки
 from loguru import logger
+import random
 
 print('Copyright © 2022 vuwWolf')
 TokenPenis = input("Введите токен: ")
-bot = Bot(token=TokenPenis)
+bot = Bot(TokenPenis)
 
 @bot.on.message(text="/help") # Задаю хуйню, чтобы при появлении "/help" в чате, писалось сообщение
 async def help_handler(message: Message):
@@ -28,6 +29,17 @@ async def staff_handler(message: Message):
     await message.answer(f'[id{users_info[0].id}|{users_info[0].first_name}], вот все разработчики сервера:\n'
                          '\n'
                          'Антон Шмагин (https://vk.com/aantoss) и Павел Одинцов (https://vk.com/errormaned)')
+
+@bot.on.message(text="/ПРИКАЗ <text>")
+async def prikaz_handler(message: Message):
+    users_info = await bot.api.users.get(message.from_id)
+    prikaz_number = random.randint(10,10000)
+    if users_info[0].id == 231416101:
+        await message.answer(f'ПО ПРИКАЗУ [id231416101|ДАНИИЛА] №{prikaz_number}')
+    else:
+        await message.answer(f'[id{users_info[0].id}|Ты] долбоёб?')
+
+
 
 
 @bot.on.message(text="/lobanov_suka") # пасхалки
